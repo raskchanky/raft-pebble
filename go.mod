808df771e18157e3ab1e7f445add12040a48dc37
Josh Black <raskchanky@users.noreply.github.com>
2021-11-19 17:46:23 -0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/raskchanky/raft-pebble
+
+go 1.17
+
+require (
+	github.com/DataDog/zstd v1.4.5 // indirect
+	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cockroachdb/errors v1.8.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/cockroachdb/pebble v0.0.0-20211110184633-447349ef50e6 // indirect
+	github.com/cockroachdb/redact v1.0.8 // indirect
+	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hashicorp/go-hclog v0.9.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-msgpack v1.1.5 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/hashicorp/raft v1.3.2 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
+	golang.org/x/sys v0.0.0-20210909193231-528a39cd75f3 // indirect
+)
---
+++ pebble_store.go
@@ -0,0 +1,241 @@
+package raftpebble
+
+import (
+	"errors"
+
+	"github.com/cockroachdb/pebble"
+	"github.com/hashicorp/raft"
+)
+
+const (
+	// Permissions to use on the db file. This is only used if the
+	// database file does not exist and needs to be created.
+	dbFileMode = 0600
+)
+
+var (
+	// An error indicating a given key does not exist
+	ErrKeyNotFound = errors.New("not found")
+)
+
+// PebbleStore provides access to pebble for Raft to store and retrieve
+// log entries. It also provides key/value storage, and can be used as
+// a LogStore and StableStore.
+type PebbleStore struct {
+	// conn is the underlying handle to the db.
+	conn *pebble.DB
+
+	// The path to the pebble database directory
+	path string
+}
+
+// Options contains all the configuration used to open the database
+type Options struct {
+	// Path is the file path
+	Path string
+
+	// PebbleOptions contains any specific pebble options you might
+	// want to specify [e.g. open timeout]
+	PebbleOptions *pebble.Options
+}
+
+// readOnly returns true if the contained bolt options say to open
+// the DB in readOnly mode [this can be useful to tools that want
+// to examine the log]
+func (o *Options) readOnly() bool {
+	return o != nil && o.PebbleOptions != nil && o.PebbleOptions.ReadOnly
+}
+
+// NewPebbleStore takes a file path and returns a connected Raft backend.
+func NewPebbleStore(path string) (*PebbleStore, error) {
+	return New(Options{Path: path})
+}
+
+// New uses the supplied options to open the pebble db and prepare it for use as a raft backend.
+func New(options Options) (*PebbleStore, error) {
+	// Try to connect
+	handle, err := pebble.Open(options.Path, options.PebbleOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the new store
+	store := &PebbleStore{
+		conn: handle,
+		path: options.Path,
+	}
+
+	return store, nil
+}
+
+// Close is used to gracefully close the DB connection.
+func (b *PebbleStore) Close() error {
+	return b.conn.Close()
+}
+
+// FirstIndex returns the first known index from the Raft log.
+func (b *PebbleStore) FirstIndex() (uint64, error) {
+	iter := b.conn.NewIter(nil)
+	defer iter.Close()
+
+	if valid := iter.First(); valid	{
+		if key := iter.Key(); key != nil {
+			return bytesToUint64(key), nil
+		}
+	}
+
+	return 0, nil
+}
+
+// LastIndex returns the last known index from the Raft log.
+func (b *PebbleStore) LastIndex() (uint64, error) {
+	iter := b.conn.NewIter(nil)
+	defer iter.Close()
+
+	if valid := iter.Last(); valid	{
+		if key := iter.Key(); key != nil {
+			return bytesToUint64(key), nil
+		}
+	}
+
+	return 0, nil
+}
+
+// GetLog is used to retrieve a log from pebble at a given index.
+func (b *PebbleStore) GetLog(idx uint64, log *raft.Log) error {
+	val, closer, err := b.conn.Get(uint64ToBytes(idx))
+
+	if err != nil {
+		if closer != nil {
+			closer.Close()
+		}
+
+		if err == pebble.ErrNotFound {
+			return raft.ErrLogNotFound
+		} else {
+			return err
+		}
+	}
+
+	err = decodeMsgPack(val, log)
+	if closer != nil {
+		closer.Close()
+	}
+
+	return err
+}
+
+// StoreLog is used to store a single raft log
+func (b *PebbleStore) StoreLog(log *raft.Log) error {
+	return b.StoreLogs([]*raft.Log{log})
+}
+
+// StoreLogs is used to store a set of raft logs
+func (b *PebbleStore) StoreLogs(logs []*raft.Log) error {
+	for _, log := range logs {
+		key := uint64ToBytes(log.Index)
+		val, err := encodeMsgPack(log)
+		if err != nil {
+			return err
+		}
+		// Note: this will sync to disk by default. Pass &pebble.WriteOptions{Sync: false}
+		// to disable this and buffer writes in memory.
+		if err := b.conn.Set(key, val.Bytes(), nil); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// DeleteRange is used to delete logs within a given range inclusively.
+func (b *PebbleStore) DeleteRange(min, max uint64) error {
+	minKey := uint64ToBytes(min)
+	bt := b.conn.NewIndexedBatch()
+
+	iter := bt.NewIter(nil)
+
+	for iter.SeekGE(minKey); iter.Valid(); iter.Next() {
+		k := iter.Key()
+
+		// Handle out-of-range log index
+		if bytesToUint64(k) > max {
+			break
+		}
+
+		// Delete in-range log index
+		if err := bt.Delete(k, nil); err != nil {
+			iter.Close()
+			return err
+		}
+	}
+
+	iter.Close()
+	return bt.Commit(nil)
+}
+
+// Set is used to set a key/value set outside of the raft log
+func (b *PebbleStore) Set(k, v []byte) error {
+	return b.conn.Set(k, v, nil)
+}
+
+// Get is used to retrieve a value from the k/v store by key
+func (b *PebbleStore) Get(k []byte) ([]byte, error) {
+	val, closer, err := b.conn.Get(k)
+
+	if err != nil {
+		if closer != nil {
+			closer.Close()
+		}
+
+		if err == pebble.ErrNotFound {
+			return nil, ErrKeyNotFound
+		} else {
+			return nil, err
+		}
+	}
+
+	result := append([]byte(nil), val...)
+
+	if closer != nil {
+		closer.Close()
+	}
+
+	return result, nil
+}
+
+// SetUint64 is like Set, but handles uint64 values
+func (b *PebbleStore) SetUint64(key []byte, val uint64) error {
+	return b.conn.Set(key, uint64ToBytes(val), nil)
+}
+
+// GetUint64 is like Get, but handles uint64 values
+func (b *PebbleStore) GetUint64(key []byte) (uint64, error) {
+	val, closer, err := b.conn.Get(key)
+
+	if err != nil {
+		if closer != nil {
+			closer.Close()
+		}
+
+		if err == pebble.ErrNotFound {
+			return 0, ErrKeyNotFound
+		} else {
+			return 0, err
+		}
+	}
+
+	result := bytesToUint64(val)
+	if closer != nil {
+		closer.Close()
+	}
+
+	return result, nil
+}
+
+// Sync performs an fsync on the database file handle. This is not necessary
+// under normal operation unless NoSync is enabled, in which this forces the
+// database file to sync against the disk.
+func (b *PebbleStore) Sync() error {
+	return b.conn.Flush()
+}
